54d87d96e1db09393de9b37316c8d24bd08346e7
Abdullah Alrasheed <twistedhardware@gmail.com>
2013-10-10 21:42:33 +0300

    improve animation, add menu

--- js/draw-svg.js
+++ js/draw-svg.js
@@ -1,5 +1,5 @@
 // Declare main objects in svg
-var svg_brand, svg_logo, svg_slogan;
+var svg_brand, svg_logo, svg_slogan, svg_menu, svg_menu_1, svg_menu_2;
 
 // Prepare SVG
 var svg = 
@@ -36,30 +36,94 @@ d3.json("json/test.json", function(json) {
 		svg_brand.selectAll(".svg-logo")
 				.data(json)
 			.enter().append("text")
-				.attr("x", "0")
-				.text("")
-			.transition() 
 				.attr("class", "svg-logo")
-				.attr("font-family", "sans-serif")
+				.attr("x", "0")
+				.attr("font-size", "45px") 
 				.attr("fill", "red")
-				.attr("x", "10")
 				.attr("y", "50")
 				.attr("text-anchor", "start")
-				.text(function(d){ return d.title; });
+				.text(function(d){ return d.title; })
+			.transition()
+				.duration(1000)
+				.attr("font-size", "45px") 
+				.attr("x", "10");
+
 	
 	//Dynamic Text Slogan
 	svg_slogan = 
 		svg_brand.selectAll(".svg-slogan")
 				.data(json)
 			.enter().append("text")
-				.attr("x", "0")
-				.text("")
-			.transition() 
 				.attr("class", "svg-slogan")
-				.attr("font-family", "sans-serif")
 				.attr("fill", "black")
-				.attr("x", "10")
-				.attr("y", "80")
+				.attr("x", "0")
+				.attr("font-size", "14px") 
+				.attr("y", "70")
 				.attr("text-anchor", "start")
-				.text(function(d){ return d.slogan; });
+				.text(function(d){ return d.slogan; })
+			.transition()
+				.duration(1000)
+				.attr("font-size", "14px") 
+				.attr("x", "10");
 });
+
+//Add g for branding
+svg_menu = 
+	svg.append("g")
+		.attr("transform", "translate(0,0)")
+		.attr("class", "svg-menu");
+
+//Static Menu Test - (Working)
+svg_menu.append("rect")
+	.attr("ry", "6")
+	.attr("rx", "6")
+	.attr("y", "94")
+	.attr("x", "0")
+	.attr("height", "40")
+	.attr("width", "1")
+	.attr("fill", "rgba(0,0,0,.05)")
+	.attr("stroke", "rgba(99,99,99,1)")
+	.attr("stroke-width", "2")
+	.attr("class", "svg-menu-item")
+	.transition()
+		.delay(500)
+		.duration(500)
+		.attr("width", "200")
+		.attr("x", "5");
+
+svg_menu.append("text")
+	.attr("class", "svg-logo")
+	.attr("font-family", "sans-serif")
+	.attr("font-size", "20px")
+	.attr("fill", "red")
+	.attr("x", "20")
+	.attr("y", "120")
+	.attr("text-anchor", "start")
+	.text("Home");
+
+svg_menu.append("rect")
+	.attr("ry", "6")
+	.attr("rx", "6")
+	.attr("y", "144")
+	.attr("x", "0")
+	.attr("height", "40")
+	.attr("width", "1")
+	.attr("fill", "rgba(0,0,0,.05)")
+	.attr("stroke", "rgba(99,99,99,1)")
+	.attr("stroke-width", "2")
+	.attr("class", "svg-menu-item")
+	.transition()
+		.delay(750)
+		.duration(500)
+		.attr("width", "200")
+		.attr("x", "5");
+
+svg_menu.append("text")
+	.attr("class", "svg-logo")
+	.attr("font-family", "sans-serif")
+	.attr("font-size", "20px")
+	.attr("fill", "red")
+	.attr("x", "20")
+	.attr("y", "170")
+	.attr("text-anchor", "start")
+	.text("Other Page");
