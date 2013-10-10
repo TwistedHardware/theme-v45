e5af94d0645239f23ad2f5f72818a6934f616a69
Abdullah Alrasheed <twistedhardware@gmail.com>
2013-10-11 00:04:14 +0300

    make menu dynamic

--- js/draw-svg.js
+++ js/draw-svg.js
@@ -73,6 +73,10 @@ svg_menu =
 		.attr("transform", "translate(0,0)")
 		.attr("class", "svg-menu");
 
+
+/**
+ 
+ 
 //Static Menu Test - (Working)
 svg_menu.append("rect")
 	.attr("ry", "6")
@@ -92,14 +96,18 @@ svg_menu.append("rect")
 		.attr("x", "5");
 
 svg_menu.append("text")
-	.attr("class", "svg-logo")
+	.attr("class", "svg-menu-text")
 	.attr("font-family", "sans-serif")
 	.attr("font-size", "20px")
-	.attr("fill", "red")
+	.attr("fill", "rgba(255,0,0,0")
 	.attr("x", "20")
 	.attr("y", "120")
 	.attr("text-anchor", "start")
-	.text("Home");
+	.text("Home")
+	.transition()
+		.delay(750)
+		.duration(500)
+		.attr("fill", "rgba(255,0,0,1");
 
 svg_menu.append("rect")
 	.attr("ry", "6")
@@ -136,14 +144,18 @@ svg_menu.append("rect")
 		.attr("x", "5");
 
 svg_menu.append("text")
-	.attr("class", "svg-logo")
+	.attr("class", "svg-menu-text")
 	.attr("font-family", "sans-serif")
 	.attr("font-size", "20px")
-	.attr("fill", "red")
+	.attr("fill", "rgba(255,0,0,0")
 	.attr("x", "20")
 	.attr("y", "170")
 	.attr("text-anchor", "start")
-	.text("Other Page");
+	.text("Other Page")
+		.transition()
+		.delay(1000)
+		.duration(500)
+		.attr("fill", "rgba(255,0,0,1");
 
 svg_menu.append("rect")
 	.attr("ry", "6")
@@ -161,3 +173,65 @@ svg_menu.append("rect")
 		.duration(500)
 		.attr("width", "200")
 		.attr("x", "5");
+
+*/
+
+//Dynamic Text Branding
+d3.json("json/menu.json", function(json) {
+	
+	//Dynamic Text Logo 
+	svg_menu.selectAll(".svg-menu-item")
+			.data(json)
+		.enter().append("rect")
+			.attr("class", "svg-menu-item")
+			.attr("rx", "6")
+			.attr("ry", "6")
+			.attr("x", "0")
+			.attr("y", function(d,i) { return (94 + (i * 50));  })
+			.attr("height", "40")
+			.attr("width", "1")
+			.attr("fill", "rgba(0,0,0,.05)")
+			.attr("stroke", "rgba(99,99,99,1)")
+			.attr("stroke-width", "2")
+			.attr("class", "svg-menu-item")
+			.transition()
+				.delay(function(d,i) { return 500 + i * 250 })
+				.duration(500)
+				.attr("width", "200")
+				.attr("x", "5");
+
+	svg_menu.selectAll(".svg-menu-text")
+			.data(json)
+		.enter().append("text")
+			.attr("class", "svg-menu-text")
+			.attr("font-size", "20px")
+			.attr("fill", "rgba(255,0,0,0")
+			.attr("x", "20")
+			.attr("y", function(d,i) { return (120 + (i * 50));  })
+			.attr("text-anchor", "start")
+			.text(function(d) { return d.title })
+				.transition()
+				.delay(function(d,i) { return 500 + i * 250 })
+				.duration(500)
+				.attr("fill", "rgba(255,0,0,1");
+	
+	svg_menu.selectAll(".svg-menu-mask")
+			.data(json)
+		.enter().append("rect")
+			.attr("class", "svg-menu-mask")
+			.attr("ry", "6")
+			.attr("rx", "6")
+			.attr("y", function(d,i) { return (94 + (i * 50));  })
+			.attr("x", "0")
+			.attr("height", "40")
+			.attr("width", "1")
+			.attr("fill", "rgba(0,0,0,.0)")
+			.attr("stroke", "rgba(99,99,99,1)")
+			.attr("stroke-width", "2")
+			.attr("class", "svg-menu-mask")
+			.transition()
+				.delay(function(d,i) { return 500 + i * 250 })
+				.duration(500)
+				.attr("width", "200")
+				.attr("x", "5");
+});
