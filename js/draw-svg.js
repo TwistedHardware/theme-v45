505133974695a8fdd3b8e8257157680ce124aea3
Abdullah Alrasheed <twistedhardware@gmail.com>
2013-11-18 01:45:31 +0300

    add chart support

--- js/draw-svg.js
+++ js/draw-svg.js
@@ -251,7 +251,7 @@ d3.json("json/menu.json", function(json) {
 svg_page = 
 	svg.append("g")
 		.attr("transform", "translate(0,0)")
-		.attr("class", "");
+		.attr("class", "svg-chart");
 
 //Adding sattic page contets
 svg_page.append("rect")
@@ -376,15 +376,23 @@ function openPage(d)
 			.attr("fill", "rgba(255,0,0,1)");
 	
 	//update page contents
-	d3.text("pages/" + d.source, function(data){
-		svg_page.select(".svg-p-text")
-			.attr("height", "1")
-			.html("<div class='svg-contents'>" + data + "</div>")
-			.transition()
-				.delay(650)
-				.duration(1000)
-				.attr("height", viewportheight - 140);
-		
+	//choose chart type
+	if (d.cType == "force")
+	{
+		drawForceChart(d.source);
+	}
+	else
+	{
+		d3.text("pages/" + d.source, function(data){
+			svg_page.select(".svg-p-text")
+				.attr("height", "1")
+				.html("<div class='svg-contents'>" + data + "</div>")
+				.transition()
+					.delay(650)
+					.duration(1000)
+					.attr("height", viewportheight - 140);
+		});
+	}
 		svg_page.select("rect")
 			.transition()
 				.duration(400)
@@ -393,7 +401,173 @@ function openPage(d)
 				.duration(1000)
 				.attr("height", viewportheight - 40);
 
-	});
+	
 
 	//alert(d.source);
+}
+
+//These functions are to draw charts
+// *** Force Chart ***
+
+
+var w,
+h,
+v,
+node,
+link,
+root,
+force,
+vis,
+tip;
+
+function drawForceChart(dataSource)
+{
+	// Hide Text
+	svg_page.select(".svg-p-text")
+		.attr("height", "1");
+	
+	w=viewportwidth - 270;
+	h=viewportheight - 140;
+	
+	v = d3.scale.linear()
+    	.domain([100000000,10000000000])
+    	.range([3, 4]);
+	
+	force = d3.layout.force()
+		.on("tick", tick)
+	    .linkDistance(60)
+	    .charge(-50)     // -20
+	    .gravity(.1)//0.1
+		.charge(function(d) { return d._children ? -d.cap / 10000000 : -30; })
+		.linkDistance(function(d) { return d.target._children ? 80 : 30; })
+		.size([w, h + 160]);
+
+	vis = d3.select(".svg-chart")
+    	.attr("width", w)
+    	.attr("height", h-160);
+	
+	tip = d3.tip()
+	    .attr('class', 'd3-tip')
+	    .offset([-10, 0])
+	    .html(function(d) { return "<div style='background-color: rgba(200,200,200,0.5);'><strong>" + d.name + "</strong><br /><span style='color:black'>" + d.sector + "</span><br /><span style='color:red'>" + d.fdate + "</span><br /><span style='color:blue'>" + d.cap + "</span></div>"; })
+    svg.call(tip);
+	
+	d3.json("pages/" + dataSource, function(json) {
+		root = json;
+		root.fixed = false;
+		root.x = w / 2;
+		root.y = h / 2 + 80;
+		update();
+	});
+}
+
+function update() {
+	  var nodes = flatten(root),
+	      links = d3.layout.tree().links(nodes);
+
+	  // Restart the force layout.
+	  force
+	      .nodes(nodes)
+	      .links(links)
+	      .start();
+
+	  // Update the links…
+	  link = vis.selectAll("line.link")
+	      .data(links, function(d) { return d.target.id; });
+
+	  // Enter any new links.
+	  link.enter().insert("svg:line", ".node")
+	      .attr("class", "link")
+	      .attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+	  // Exit any old links.
+	  link.exit().remove();
+
+	  // Update the nodes…
+	  node = vis.selectAll("circle.node")
+	      .data(nodes, function(d) { return d.id; })
+	      .style("fill", color);
+
+	  node.transition()
+	      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.cap) / 20000; });
+
+	  // Enter any new nodes.
+	  var nodeEnter = node.enter().append("svg:circle")
+	      .attr("class", "node")
+	      .attr("cx", function(d) { return d.x; })
+	      .attr("cy", function(d) { return d.y; })
+	      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.cap) / 20000; })
+          .on("mouseover", tip.show)
+          .on("mouseout", tip.hide)
+	      .style("fill", color)
+	      .on("click", click)
+	      .call(force.drag);
+
+	nodeEnter.append("circle")
+		.attr("r", function(d) { return v(d.cap) || 4.5; });
+	  
+	nodeEnter.append("text")
+      	.attr("x", 17)
+      	.attr("dy", ".35em")
+      	.style("font", "7px sans-serif")
+      	.text(function(d) { return d.name; });
+	
+	node.select("circle")
+    	.style("fill", color);
+      
+	  // Exit any old nodes.
+	  node.exit().remove();
+	}
+
+function tick() {
+	link
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+	node.attr("cx", function(d) {
+		return d.x;
+	}).attr("cy", function(d) {
+		return d.y;
+	});
+}
+
+// Color leaf nodes orange, and packages white or blue.
+function color(d) {
+	return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+}
+
+// Toggle children on click.
+function click(d) {
+	if (d.children) {
+		d._children = d.children;
+		d.children = null;
+	} else {
+		d.children = d._children;
+		d._children = null;
+	}
+	update();
+}
+
+// Returns a list of all nodes under the root.
+function flatten(root) {
+	var nodes = [], i = 0;
+
+	function recurse(node) {
+		if (node.children)
+			node.size = node.children.reduce(function(p, v) {
+				return p + recurse(v);
+			}, 0);
+		if (!node.id)
+			node.id = ++i;
+		nodes.push(node);
+		return node.size;
+	}
+
+	root.size = recurse(root);
+	return nodes;
 }
